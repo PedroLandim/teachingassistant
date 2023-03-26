2b346873348d7296409ecc5d3ec18a0a59d3c87c
Pedro Landim <86279678+PedroLandim@users.noreply.github.com>
2023-03-26 19:13:45 -0300

    adicionando o ta-server e o common

--- ta-server/ta-server.ts
+++ ta-server/ta-server.ts
@@ -1,16 +1,42 @@
-import express from 'express';
+import express = require('express');
+import bodyParser = require("body-parser");
 
-const taserver = express();
+import {Aluno} from '../common/aluno';
+import {CadastroDeAlunos} from './cadastrodealunos';
 
-const alunos = [
-  { nome: 'Paulo', cpf: '683', email: 'phmb@cin.br', metas: { 'requisitos': 'MA' } },
-  { nome: 'Mariana', cpf: '456', email: '@mcb@cin.br', metas: { 'requisitos': 'MPA' } }
-];
+var taserver = express();
 
-taserver.get('/', function (req, res) {
-  res.send(alunos);
-});
+var alunos: CadastroDeAlunos = new CadastroDeAlunos();
+
+taserver.use(bodyParser.json());
+
+taserver.get('/alunos', function (req, res) {
+  var aluno: string = JSON.stringify(alunos.getAlunos());
+  res.send(aluno);
+})
+
+taserver.post('/aluno', function (req: express.Request, res: express.Response) {
+  var aluno: Aluno = <Aluno> req.body; //verificar se é mesmo Aluno!
+  aluno = alunos.criar(aluno);
+  if (aluno) {
+    res.send({"success": "O aluno foi cadastrado com sucesso"});
+  } else {
+    res.send({"failure": "O aluno não pode ser cadastrado"});
+  }
+})
+
+taserver.put('/aluno', function (req: express.Request, res: express.Response) {
+  var aluno: Aluno = <Aluno> req.body;
+  aluno = alunos.atualizar(aluno);
+  if (aluno) {
+    res.send({"success": "O aluno foi atualizado com sucesso"});
+  } else {
+    res.send({"failure": "O aluno não pode ser atualizado"});
+  }
+})
 
 taserver.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+
+  console.log('Example app listening on port 3000!')
+
+})
